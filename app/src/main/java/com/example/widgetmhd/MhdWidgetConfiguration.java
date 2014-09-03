1b8269ca84777716ea746c07b0ce1f18c2affa39
boni11 <lukas.stefanko@gmail.com>
2014-09-03 11:33:09 +0200

    added configuration menu

--- app/src/main/java/com/example/widgetmhd/MhdWidgetConfiguration.java
+++ app/src/main/java/com/example/widgetmhd/MhdWidgetConfiguration.java
@@ -1,17 +1,54 @@
 package com.example.widgetmhd;
 
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
 
 
 public class MhdWidgetConfiguration extends ActionBarActivity {
+
+    private int widgetId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mhd_widget_configuration);
+        setResult(RESULT_CANCELED);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        AppWidgetManager widgetManager=AppWidgetManager.getInstance(this);
+        RemoteViews views=new RemoteViews(this.getPackageName(),R.layout.mhd_widget_layout);
+
+        //zavolat fciu nech nastavy spinery
+
+        Button uloz= (Button) findViewById(R.id.uloz_config);
+        uloz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dostat zastavky zo spinerov
+                //ulozit do shared preferences
+
+                //bud posli intent pre Receiver alebo len zavolaj fciu
+
+                Intent resultValue=new Intent();
+                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,widgetId);
+                setResult(RESULT_OK,resultValue);
+                finish();
+
+            }
+        });
+
+
     }
 
 
